66a3cbc1f42d1aed1830844086e0da0b827b9a29
rick91236rick <rick91236rick@gmail.com>
2022-04-01 17:32:10 +0800

    add test

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -4,6 +4,9 @@ namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
 use TripServiceKata\Trip\TripService;
+use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\User\User;
+use TripServiceKata\User\UserSession;
 
 class TripServiceTest extends TestCase
 {
@@ -17,8 +20,28 @@ class TripServiceTest extends TestCase
         $this->tripService = new TripService;
     }
 
-    /** @test */ public function 
-    it_does_something() {
-        $this->fail('This test has not been implemented yet.');
+    /** @test */
+    public function should_Throw_Exception_When_User_Is_Not_LoggedIn()
+    {
+        $user = new User("rick");
+        $this->tripService->getTripsByUser($user);
+        $this->expectException(UserNotLoggedInException::class);
+    }
+
+    /** @test */
+    public function should_Not_Return_Trips_When_Logged_User_Are_Not_Friend()
+    {
+        $user = new User("rick");
+        $tripList = $this->tripService->getTripsByUser($user);
+        $this->assertCount(0, $tripList);
+    }
+
+
+    /** @test */
+    public function should_Return_Trips_When_Logged_User_Are_Friend()
+    {
+        $user = new User("rick");
+        $tripList = $this->tripService->getTripsByUser($user);
+        $this->assertNotCount(0,$tripList);
     }
 }
